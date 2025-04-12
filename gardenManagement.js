3cc459edbc77c4bdd482af51571d07cd04a50eda
Blake Godsey <somethingclever182@gmail.com>
2025-04-11 20:49:29 -0500

    if watering/if lights/messaure soil

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,35 @@
 const temperature = 90;
 const timeOfDay = "morning";
+let lightStatus 
 let soilMoisture = 30;
+
+
+//conditional statements
+if (temperature > 80 ){
+    console.log("Watering on");
+
+//if-else statement
+} else {
+    console.log("Watering off");
+}
+
+//time of day lights
+if (timeOfDay === "evening") {
+    lightStatus = "Lights on";
+}
+else if (timeOfDay === "night") {
+    lightStatus = "Lights on";
+}
+else {
+    lightStatus = "Lights off";
+}
+console.log (lightStatus);
+
+
+//loops 
+while (soilMoisture < 40) {
+    soilMoisture +=5; 
+
+}
+console.log(soilMoisture);
+//for loop  
